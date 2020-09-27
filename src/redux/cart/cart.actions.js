362d7fb29c04b458cd2dbce90e390f55083089c7
Mohamed Hussein <mohamed.hussein151515@gmail.com>
2020-09-27 13:40:50 +0200

    Updated cart-utils to group the same items

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,12 @@
 import cartActionTypes from './cart.types'
 
+const { TOGGLE_CART_HIDDEN, ADD_ITEM } = cartActionTypes;
+
 export const ToggleCartHidden = () => ({
-  type: cartActionTypes.TOGGLE_CART_HIDDEN
+  type: TOGGLE_CART_HIDDEN
+})
+
+export const addItem = item => ({
+  type: ADD_ITEM,
+  payload: item
 })
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,15 +1,26 @@
 import cartAcionTypes from "./cart.types";
+import { addItemToCart } from './cart.utils';
 
 const initialState = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 }
 
 const cartRedecer = (state = initialState, action) => {
-  switch (action.type) {
-      case cartAcionTypes.TOGGLE_CART_HIDDEN:
+  const { TOGGLE_CART_HIDDEN, ADD_ITEM } = cartAcionTypes;
+  const { hidden, cartItems } = state;
+  const { type, payload } = action;
+
+  switch (type) {
+      case TOGGLE_CART_HIDDEN:
         return {
           ...state,
-          hidden: !state.hidden
+          hidden: !hidden
+        }
+      case ADD_ITEM: 
+        return {
+          ...state,
+          cartItems: addItemToCart(cartItems, payload)
         }
       default:
         return state
